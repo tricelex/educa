b889433ffb09c863e2b4b0b762d854e945b80df5
emmanuelokoye <emmanuelokoye@quickcheck.ng>
2021-07-20 12:32:22 +0100

    Fuxed course urls

--- courses/models.py
+++ courses/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
+
 from .fields import OrderField
 
 
--- educa/urls.py
+++ educa/urls.py
@@ -18,7 +18,7 @@ from django.contrib.auth import views as auth_views
 from django.urls import path
 
 urlpatterns = [
-    path("accounts/login/", auth_views.LoginView.as_view(), name='login'),
-    path("accounts/logout/", auth_views.LogoutView.as_view(), name='logout'),
+    path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
+    path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("admin/", admin.site.urls),
 ]
